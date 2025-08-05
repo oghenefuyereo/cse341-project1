682e2e33515fb18fa28750d26bcb49f5bf97b808
oghenefuyereo <oghenefuyereo@gmail.com>
2025-08-04 22:39:39 -0400

    update

--- swagger.js
+++ swagger.js
@@ -1,17 +1,18 @@
 const swaggerAutogen = require("swagger-autogen")();
 
+const host = process.env.HOST || "localhost:3000";
+
 const doc = {
   info: {
     title: "Users API",
     description: "Users API",
   },
-  host: process.env.HOST || "localhost:3000",
-  schemes: ["http", "https"],
+  host: host,
+  schemes: host.includes("localhost") ? ["http"] : ["https"],
 };
 
 const outputFile = "./swagger.json";
 const endpointsFiles = ["./routes/index.js"];
-
 
 swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
   require("./server"); 
