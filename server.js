57308892fcca02cfb135f2b7738ac8a448e4d63f
oghenefuyereo <oghenefuyereo@gmail.com>
2025-08-04 20:57:08 -0400

    update

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongodb = require("./data/database");
-const bodyParser = require("body-parser");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
-app.use(bodyParser.json());
+// Use built-in JSON parser BEFORE routes
+app.use(express.json());
+
+app.use("/", require("./routes"));
 
 mongodb.initDb((err) => {
   if (err) {
